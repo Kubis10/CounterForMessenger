cc2d2cf110b92e17003ffc9dd900e66b87a80048
Kuba Przybysz <50967586+Kubis10@users.noreply.github.com>
2022-02-10 23:26:52 +0100

    Counting multiply files

--- Main.py
+++ Main.py
@@ -1,11 +1,17 @@
 import os
 import json
 
-print(os.listdir('data/messages/inbox/'))
-f = open('data/messages/inbox/18urodzinyandziuliny_jolrt76jwq/message_1.json', 'r')
-data = json.load(f)
-totalNum = 0
-for i in data['messages']:
-    totalNum += 1
-print(totalNum)
-f.close()
+def countPerPerson(data):
+    totalNum = 0
+    for i in os.listdir('data/messages/inbox/'+data):
+        with open('data/messages/inbox/' + i + "/" + j, 'r') as f:
+            data = json.load(f)
+            for i in data['messages']:
+                totalNum += 1
+    f.close()
+    return totalNum
+
+
+for i in os.listdir('data/messages/inbox/'):
+    print(countPerPerson(i))
+
