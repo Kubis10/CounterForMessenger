13502b17fa8c44e4114479f5140f46ef88619ec8
Kuba Przybysz <50967586+Kubis10@users.noreply.github.com>
2022-02-11 09:40:44 +0100

    Added tkinter

--- Main.py
+++ Main.py
@@ -1,6 +1,14 @@
 import os
 import json
 import glob
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
 
 ile = 0
 
@@ -14,9 +22,11 @@ def countPerPerson(data, path):
             for l in data['messages']:
                 totalNum += 1
             for k in data['participants']:
-                if k['name'] not in parcipants and k['name'] != 'Kuba Przybysz':
+                if k['name'] not in parcipants:
                     parcipants.append(k['name'].encode('iso-8859-1').decode('utf-8'))
-    return parcipants, totalNum
+            title = data['title'].encode('iso-8859-1').decode('utf-8')
+            thread_type = data['thread_type']
+    return title, thread_type, totalNum
 
 
 for i in os.listdir('data/messages/inbox/'):
