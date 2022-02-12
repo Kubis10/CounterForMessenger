0106db777f1cf1734824b923057306fadbd2b6ad
Kuba Przybysz <50967586+Kubis10@users.noreply.github.com>
2022-02-12 01:26:28 +0100

    Added tkinter

--- Main.py
+++ Main.py
@@ -7,28 +7,39 @@ from tkinter import ttk
 root = Tk()
 root.title("Counter for messenger")
 root.iconbitmap(r'D:\Projekty\CounterForMessenger\assets\CFM.ico')
-root.geometry("900x600")
+root.geometry("1024x700")
+root.configure(background='#232323')
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+s = ttk.Style()
+s.configure('TFrame', background='#131313')
+s.configure('TButton', background='#03cafc')
+nav = ttk.Frame(root, padding=10, style='s.TFrame')
+ttk.Button(nav, text="Wskaż ścieżkę do folderu").grid(row=0, column=0, sticky="ew", padx=5)
+ttk.Button(nav, text="Ustawienia").grid(row=1, column=0, sticky="ew", padx=5)
+ttk.Button(nav, text="Wyjście", command=root.destroy).grid(row=2, column=0, sticky="ew", padx=5)
+nav.grid(row=0, column=0, sticky="ns")
 root.mainloop()
 
 ile = 0
+numMess = 0
 
 
 def countPerPerson(data, path):
+    global numMess
     totalNum = 0
-    parcipants = []
+    participants = []
     result = glob.glob(path + data + '/*.json')
     for j in result:
         with open(j, 'r') as f:
             data = json.load(f)
             for l in data['messages']:
                 totalNum += 1
+                if l['sender_name'] == "Kuba Przybysz":
+                    numMess += 1
             for k in data['participants']:
-                if k['name'] not in parcipants:
-                    parcipants.append(k['name'].encode('iso-8859-1').decode('utf-8'))
+                if k['name'] not in participants:
+                    participants.append(k['name'].encode('iso-8859-1').decode('utf-8'))
             title = data['title'].encode('iso-8859-1').decode('utf-8')
             thread_type = data['thread_type']
     return title, thread_type, totalNum
@@ -40,6 +51,7 @@ def countAll(ile, path):
         print(conf)
         ile += 1
     print(ile)
+    print(numMess)
 
 
 # countAll(ile, 'data/messages/inbox/')
