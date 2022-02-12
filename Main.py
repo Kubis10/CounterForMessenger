767971cf872058d1051ccb7308433c840b78df00
Kuba Przybysz <50967586+Kubis10@users.noreply.github.com>
2022-02-12 15:58:27 +0100

    Added sorting by messages

--- Main.py
+++ Main.py
@@ -26,15 +26,27 @@ def countPerPerson(data, path):
                     participants.append(k['name'].encode('iso-8859-1').decode('utf-8'))
             title = data['title'].encode('iso-8859-1').decode('utf-8')
             thread_type = data['thread_type']
-    return title, thread_type, totalNum
+    return title, participants, thread_type, totalNum
+
+
+def treeview_sort_column(tv, col, reverse):
+    l = [(tv.set(k, col), k) for k in tv.get_children('')]
+    l.sort(key=lambda t: int(t[0]), reverse=reverse)
+
+    for index, (val, k) in enumerate(l):
+        tv.move(k, '', index)
+
+    tv.heading(col,
+               command=lambda: treeview_sort_column(tv, col, not reverse))
 
 
 def countAll(path):
     ile = 0
     for i in os.listdir(path):
         conf = countPerPerson(i, path)
-        t.insert('end', str(conf[0]) + " " + str(conf[2]) + '\n')
+        t.insert(parent='', index=END, values=(conf[0], conf[1], conf[2], conf[3]))
         ile += 1
+    t.heading('msg', command=lambda _col='msg': treeview_sort_column(t, _col, False))
     print(ile)
     print(numMess)
 
@@ -53,6 +65,7 @@ root.configure(background='#232323')
 s = ttk.Style()
 s.configure('Nav.TFrame', background='#131313')
 s.configure('Main.TFrame', background='#232323')
+s.configure('Custom.Treeview', background='#232323', foreground='#ffffff')
 nav = ttk.Frame(root, padding=20, style='Nav.TFrame')
 main = ttk.Frame(root, style='Main.TFrame')
 home_icon = PhotoImage(file='./assets/home.png')
@@ -61,8 +74,14 @@ exit_icon = PhotoImage(file='./assets/exit.png')
 vis_icon = PhotoImage(file='./assets/visible.png')
 inv_icon = PhotoImage(file='./assets/invisible.png')
 v = Scrollbar(main)
-t = Text(main, width=15, wrap=NONE, yscrollcommand=v.set, background='#232323', foreground='#ffffff')
-
+t = ttk.Treeview(main, height=20, yscrollcommand=v.set, style='Custom.Treeview')
+t.column("#0", width=0,  stretch=NO)
+t['columns'] = ('name', 'pep', 'type', 'msg')
+t.heading("name", text="Nazwa", anchor=CENTER)
+t.heading("pep", text="Uczestnicy", anchor=CENTER)
+t.heading("type", text="Typ", anchor=CENTER)
+t.heading("type", text="Typ", anchor=CENTER)
+t.heading("msg", text="Liczba wiadomości", anchor=CENTER)
 ttk.Button(nav, image=home_icon, text="Strona głowna", compound=LEFT, padding=5).pack(side=TOP, pady=10)
 ttk.Button(nav, image=vis_icon, text="Pokaż wiadomości", compound=LEFT, padding=5,
            command=lambda: countAll(pathDir)).pack(side=TOP, pady=10)
@@ -71,7 +90,7 @@ ttk.Button(nav, image=settings_icon, text="Ustawienia", compound=LEFT, padding=5
 ttk.Label(main, text="Liczba wiadomości: ", foreground='#ffffff', background='#232323', font=('Arial', 15)).pack(
     side=TOP, pady=10)
 v.pack(side=RIGHT, fill=Y)
-t.pack(side=TOP, fill=BOTH, expand=True)
+t.pack(side=LEFT, fill=BOTH, expand=1)
 v.config(command=t.yview)
 nav.pack(side=LEFT, fill=Y)
 main.pack(side=RIGHT, fill=BOTH, expand=True)
